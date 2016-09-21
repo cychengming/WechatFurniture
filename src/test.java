9650979d24c88736283efffcc487e1fb4a52b7f7
cychengming <cychengming@163.com>
2016-09-21 11:06:17 +0800

    src/.

---
+++ src/test.java
@@ -0,0 +1,18 @@
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String someString = "foo"\u000A ;
+		System.out.println(someString);
+		
+		  if ( false == true ) { //these characters are magic: \u000a\u007d\u007b
+	            System.out.println("false is true!");
+	        }
+		   
+	}
+
+}
